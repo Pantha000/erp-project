5c8bd0a73889be0a5e606142de3c18a4fca9b8b3
new name <email for my second account>
2024-03-19 19:16:17 +0600

    first commit

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,12 +4,12 @@ const { google } = require("googleapis");
 const REDIRECT_URL = "https://developers.google.com/oauthplayground";
 
 const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
   REDIRECT_URL
 );
 
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 const sendMail = async (options) => {
   try {
