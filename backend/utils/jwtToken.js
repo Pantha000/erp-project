a721e1ee9066fa33e6d20b10c32913d154facf7d
new name <email for my second account>
2024-03-20 16:02:48 +0600

    first commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,6 +4,7 @@ const sendToken = (user, role, statusCode, res) => {
     const token = user.getJWTToken();
     //options for cookie
     const options = {
+      domain: "https://www.virexbd.com",
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
